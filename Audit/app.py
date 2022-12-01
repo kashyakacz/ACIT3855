2dafb8537b0e8d19efc49afa384a4e62f4bab981
Kashya Kaczkowska <73254816+kashyakacz@users.noreply.github.com>
2022-12-01 13:48:44 -0800

    update

--- Audit/app.py
+++ Audit/app.py
@@ -74,4 +74,4 @@ app.app.config['CORS_HEADERS']='Content-Type'
 app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
-     app.run(port=9080)
+     app.run(port=8110)
