02d8f4f4f8379ac1bf39c1d110f1613c5b0b970b
Kashya Kaczkowska <73254816+kashyakacz@users.noreply.github.com>
2022-12-03 14:23:35 -0800

    Lab10 update

--- Storage/app.py
+++ Storage/app.py
@@ -14,16 +14,29 @@ from calories_burned import CaloriesBurned
 from pykafka import KafkaClient
 from pykafka.common import OffsetType
 from threading import Thread 
+import os
 
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
 
-with open('app_conf.yml', 'r') as f:
+with open(app_conf_file, 'r') as f:
   app_config = yaml.safe_load(f.read())
 
-with open('log_conf.yml', 'r') as f:
-  log_config = yaml.safe_load(f.read())
-  logging.config.dictConfig(log_config)
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
 
 logger = logging.getLogger('basicLogger')
+
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
 
 DB_ENGINE = create_engine('mysql+pymysql://root:password@acit3855-lab6.westus3.cloudapp.azure.com:3306/events')
 Base.metadata.bind = DB_ENGINE
